cf77df2764c6a230f90250987bc04c689f2f69b5
Bhooshan <25bcae16@kristujayanti.com>
2026-02-10 23:12:15 +0530

    Fix port for Render

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
   res.send("Backend running successfully");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
